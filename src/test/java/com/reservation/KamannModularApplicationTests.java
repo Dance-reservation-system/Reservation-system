07c280560cd275ef58d2223a0d9ac6cfad872464
jkuznik <janusz.kuznik89@gmail.com>
2025-05-28 21:52:33 +0200

    Spring on board

---
+++ src/test/java/com/reservation/KamannModularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamannModularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
