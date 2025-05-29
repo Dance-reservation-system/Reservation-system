0b57d44f8b837e1012cef8b11b36398a3ac9d359
sarteip1 <p.litwin99@gmail.com>
2025-05-29 23:26:19 +0200

    Finished 1 version

--- src/main/java/com/reservation/KamannModularApplication.java
+++ src/main/java/com/reservation/KamannModularApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KamannModularApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KamannModularApplication.class, args);
     }
-
 }
