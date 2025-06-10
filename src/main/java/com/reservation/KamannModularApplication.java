ada0965337b25e9d998bc312a5a7c4015cba5f71
jkuznik <janusz.kuznik89@gmail.com>
2025-06-10 21:17:55 +0200

    Update code follow by suggestion

--- src/main/java/com/reservation/KamannModularApplication.java
+++ src/main/java/com/reservation/KamannModularApplication.java
@@ -1,9 +1,12 @@
 package com.reservation;
 
+import com.reservation.security.oauth.OauthProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OauthProperties.class)
 public class KamannModularApplication {
     public static void main(String[] args) {
         SpringApplication.run(KamannModularApplication.class, args);
