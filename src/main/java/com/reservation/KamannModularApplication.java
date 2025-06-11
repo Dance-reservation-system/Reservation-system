332870786f25d057a28a06fc4fc7e96cae28b60d
jkuznik <janusz.kuznik89@gmail.com>
2025-06-11 18:24:37 +0200

    Rollback commit

--- src/main/java/com/reservation/KamannModularApplication.java
+++ src/main/java/com/reservation/KamannModularApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(OauthProperties.class)
 public class KamannModularApplication {
     public static void main(String[] args) {
         SpringApplication.run(KamannModularApplication.class, args);
--- src/main/java/com/reservation/security/oauth/OauthConfig.java
+++ src/main/java/com/reservation/security/oauth/OauthConfig.java
@@ -1,10 +1,12 @@
 package com.reservation.security.oauth;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
+@EnableConfigurationProperties(OauthProperties.class)
 public class OauthConfig {
 
     @Bean
