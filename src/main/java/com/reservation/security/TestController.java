da96807607fe87d8d1441e2601384791cf59e29a
Janusz Kuźnik <151467842+jkuznik@users.noreply.github.com>
2025-06-11 20:16:51 +0200

    20 - Add Keycloack configuration: back-end rsource server setup
    * Add dependency, refactor config to achieve runnable app with oauth provider
    
    * Update docker compose config to achieve configured realm on keycloak container
    
    * Working prototype
    
    * Update configuration
    
    * Change docker files directory
    
    * Update properties
    
    * Update TestController
    
    * Update code follow by coderabbit advice
    
    * Add dependency
    
    * Prepare keycloak testcontainer with configured client secret
    
    * Test coverage for keycloak provider
    
    * Update code follow by suggestion
    
    * Rollback commit
    
    * Refactor: Rename RestClient bean method to keycloakClient for clearer Spring bean identification

---
+++ src/main/java/com/reservation/security/TestController.java
@@ -0,0 +1,24 @@
+package com.reservation.security;
+
+import com.reservation.security.oauth.KeycloakTokenProvider;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Profile("keycloak-test")
+@RestController
+@RequestMapping()
+public class TestController {
+
+    private final KeycloakTokenProvider tokenProvider;
+
+    public TestController(KeycloakTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @GetMapping("/test")
+    public String token() {
+        return tokenProvider.getAccessToken();
+    }
+}
---
+++ src/main/java/com/reservation/security/oauth/OauthConfig.java
@@ -0,0 +1,23 @@
+package com.reservation.security.oauth;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+@EnableConfigurationProperties(OauthProperties.class)
+public class OauthConfig {
+
+    @Bean
+    public RestClient keycloakClient(OauthProperties oauthProperties) {
+        return RestClient.builder()
+                .baseUrl(oauthProperties.getBaseUrl())
+                .build();
+    }
+
+    @Bean
+    public KeycloakTokenProvider keycloakTokenProvider(RestClient restClient, OauthProperties oauthProperties) {
+        return new KeycloakTokenProvider(restClient, oauthProperties);
+    }
+}
